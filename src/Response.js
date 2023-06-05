1c75d7360e174482ee8bc3a30bc7e925899cb243
mariakisel16 <maria.kisel16@yahoo.com>
2023-06-04 21:38:32 -0400

    updated

--- src/Response.js
+++ src/Response.js
@@ -2,7 +2,6 @@ import React from "react";
 import Meanings from "./Meanings";
 
 export default function Response(props) {
-  console.log(props.result);
   if (props.result) {
     return (
       <div className="result">
